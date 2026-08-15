9c33c22e234f07c29cc9e3ca265e0e257bdb6e18
Omer Banvolgyi <omerbanvo@gmail.com>
2026-08-15 09:08:16 +0300

    binary conversion

---
+++ binary_conversion.py
@@ -0,0 +1,12 @@
+def decimal_to_binary():
+    pass
+
+def apply_two_complement():
+    pass
+
+
+try:
+    regular_number = int(input("enter a decimal number:\n"))
+    bit_size = int(input("enter amount of bites"))
+except: 
+    print("you can input only numbers.")
