4e9b517c2811bd437d2202775da4b06c541eae7c
Omer Banvolgyi <omerbanvo@gmail.com>
2026-08-15 08:55:38 +0300

    home work - threads

--- learning_about_threads.py
+++ learning_about_threads.py
@@ -1,6 +1,7 @@
 import threading
 
 Counter = 0
+#פעולה שמעלה את המשתנה הגלובלי ב 100,000
 def Increase():
     global Counter
     for i in range(100000):
@@ -8,15 +9,13 @@ def Increase():
 
 thread1 = threading.Thread(target= Increase)
 
+#פעולה שמחסירה מהמשתנה הגלובלי 100,000ד
 def Decrese():
     global Counter
     for i in range(100000):
         Counter -=1
 
 thread2 = threading.Thread(target = Decrese)
-
-
-
 thread1.start()
 thread2.start()
 thread1.join()
