f0eb5267146a93904e7137f8ab357ddc9c9cf1ed
Omer Banvolgyi <omerbanvo@gmail.com>
2026-08-18 23:49:19 +0300

    improving the simple server-client to a safe upload or download program

--- client.py
+++ client.py
@@ -1,4 +1,59 @@
 import socket
+import os
+def recv_exact(sock, recv_size):
+    buffer = b""
+    while len(buffer) < recv_size:
+        chunk = sock.recv(recv_size - len(buffer))
+        if not chunk:
+            raise ConnectionError("connection faild\nconnection stopped unexpectedly before recieveing expected data ")
+        buffer += chunk
+    return buffer
+
+def upload_file(sock):
+    string = "upload request"
+    string = string.encode('utf-8')
+    stringL = len(string).to_bytes(4, "big")
+    sock.sendall(stringL)
+    sock.sendall(string)
+    file_path = str(input("write the file path you want to upload\n"))
+    if not os.path.exists(file_path):
+        print("file doesnt exist, please check again")
+        return
+    with open(file_path, "rb") as file:
+        full_file = file.read()
+        file_size = len(full_file)
+        first_4_bytes_file = file_size.to_bytes(4, "big")
+        file_name = os.path.basename(file_path)
+        
+        
+        
+    file_name = file_name.encode()
+    file_name_size = len(file_name)
+    first_4_bytes_filename = file_name_size.to_bytes(4)
+    #send first 4 bytes for the file name
+    sock.sendall(first_4_bytes_filename)
+    #send the file name
+    sock.sendall(file_name)
+    #send first 4 bytees of the file size 
+    sock.sendall(first_4_bytes_file)
+    #send the entire file 
+    sock.sendall(full_file)
+
+
+#function for downloading a file
+def recieving_file(sock, folder, name):
+    file_name = name
+    file_size_4_first_bytes = int.from_bytes(recv_exact(sock, 4), "big")
+    full_file = recv_exact(sock, file_size_4_first_bytes)
+    file_path = os.path.join(folder, name)
+    with open(file_path, 'wb') as file:
+        file.write(full_file)
+    print(f"file{file_name} recieved and stored in - {folder}")
+
+
+
+
+
 
 def start_client():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -8,12 +63,38 @@ def start_client():
     client_socket.connect((s_HOST, s_PORT))
     print(f"connected to a server - {s_HOST}: {s_PORT}")
     while True:
-        messege = str(input("what do you want to send ? (type - 'exit' if you want to exit) \n"))
+        messege = str(input("would you like to upload a file or download one? (type - 'exit' if you want to exit) \n"))
         if (messege.lower().strip() == "exit"):
             break
-        final_messege = messege.encode('utf-8')
-        client_socket.sendall(final_messege)
+        elif (messege.lower().strip() == "upload"):
+            upload_file(client_socket)
+        elif messege.lower().strip() == "download":
+            string = "download request"
+            string = string.encode('utf-8')
+            stringL = len(string).to_bytes(4, "big")
 
+            client_socket.sendall(stringL)
+            client_socket.sendall(string)
+
+            file_name = str(input("whats the name of the file you want to download?\n"))
+            file_name = file_name.encode('utf-8')
+            file_name_size_first_4_bytes = len(file_name).to_bytes(4, "big")
+
+            client_socket.sendall(file_name_size_first_4_bytes)
+            client_socket.sendall(file_name)
+
+            has_file_size = int.from_bytes(recv_exact(client_socket, 4), "big")
+            has_file = recv_exact(client_socket, has_file_size).decode('utf')
+            if has_file.lower().strip() == "file not found":
+                print("file was not found in the server's storage, please try another name")
+                continue
+            else:
+                where_to_save = str(input("where do you want to save the file?\nwrite the path..."))
+                while not os.path.exists(where_to_save):
+                    print("not found, check again")
+                recive_file_for_download(client_socket, where_to_save, file_name)
+
+        
         data = client_socket.recv(1024)
 
         returned_messege = data.decode('utf-8')
--- server.py
+++ server.py
@@ -1,10 +1,88 @@
 import socket
+import os
+from pathlib import Path
+#function that revieves the exact amount of bytes as promiesed
+def recv_exact(sock, num_bytes):
+    buffer = b""
+    while len(buffer)<num_bytes:
+        chunk = sock.recv(num_bytes-len(buffer))
+        if not chunk:
+            #the connection stops
+            raise ConnectionError("socket closed before expected data was received")
+        buffer+=chunk
+    return buffer
+
+
+
+
+#function for recieving files
+def recieving_file(sock, Ufolder):
+    #recieving the file name length
+    file_name_length = int.from_bytes(recv_exact(sock, 4), "big")
+    if not file_name_length:
+        return
+    #recieving file name
+    file_name = recv_exact(sock, file_name_length).decode('utf-8')
+    #recieving full file size
+    file_size = int.from_bytes(recv_exact(sock, 4), "big")
+    if not file_size:
+        return
+    #recieving full file
+    full_file = recv_exact(sock, file_size)
+    full_path = os.path.join(Ufolder, file_name )
+    #uploading file the the folder - upload_files_to_server
+    with open(full_path, "wb") as file:
+        file.write(full_file)
+    print(f"file{file_name} recieved and stored in - {Ufolder}")
+
+
+#function that checks if a folder has a file with a file name
+def check_if_has_file(file_name, folder_path):
+    folder = Path(folder_path)
+    for item in folder.iterdir():
+        if item.is_file():
+            if item.name == file_name:
+                return True
+            
+        elif item.is_dir():
+            if check_if_has_file(file_name, item):
+                return True
+
+    return False
+
+
+
+#function that returns a file path from a name and a foler, if after check already exists.
+def return_file_path(name, folder_path):
+    folder = Path(folder_path)
+    for item in folder.iterdir():
+        if item.is_file():
+            if item.name == name:
+                return Path(item)
+        elif item.is_dir():
+            if return_file_path(name, item):
+                return Path(item)
+        else:
+            raise KeyError('file not found')
+
+
+#function that sends a file for the user to download:
+def send_file(sock, file_path):
+    with open(file_path, "rb") as file:
+        full_file = file.read()
+        file_size_4_first_bytes = len(full_file).to_bytes(4, "big")
+        sock.sendall(file_size_4_first_bytes)
+        sock.sendall(full_file)
+
 
 def run_server():
     HOST = "127.0.0.1"
     PORT = 12345
     #create a socket - server
     socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    UPLOAD_FOLDER = '/Users/omerbanvolgyi/Documents/programming_academy/UPLOAD_FILES_TO_SERVER'
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
 
     socket_server.bind((HOST, PORT))
     print("waiting for connections...")
@@ -15,16 +93,32 @@ def run_server():
         user_socket, user_address = socket_server.accept()
         print(f"connection made with {user_address}: {user_socket}")
         while True:
-            data = user_socket.recv(1024)
-            if not data:
-                break
+            flag_length = int.from_bytes(recv_exact(user_socket, 4), "big")
+            flag = recv_exact(user_socket, flag_length).decode('utf-8')
 
-            messege = data.decode('utf-8')
+            if flag == "upload request":
+                recieving_file(user_socket, UPLOAD_FOLDER)
+            elif flag == "download request":
 
-            print(f"messege recieved from {user_address}: '{messege}'")
-            string = "messege recieved"
-            return_messege = string.encode('utf-8')
-            user_socket.send(return_messege)
+                has_file_name_size = int.from_bytes(recv_exact(user_socket, 4), "big")
+                has_file_name = recv_exact(user_socket, has_file_name).decode('utf-8')
+                has_file = check_if_has_file(has_file_name, UPLOAD_FOLDER)
+                if has_file:
+                    string = "file found".encode('utf-8')
+                    string_size_first_4_bytes = len(string).to_bytes(4, "big")
+                    user_socket.sendall(string_size_first_4_bytes)
+                    user_socket.sendall(string)
+                    file_path = return_file_path(has_file_name, UPLOAD_FOLDER)
+                    send_file(user_socket, file_path)
+
+
+
+            
+
+            #print(f"file recieved from {user_address}: '{file_name}'")
+            #string = f"file: {file_name} recieved"
+            #return_messege = string.encode('utf-8')
+            #user_socket.send(return_messege)
 
 
 run_server()
